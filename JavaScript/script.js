8637d778b1b1025ef5a623060948f2225b0f0611
wdevweb <wdevweb23@gmail.com>
2023-06-12 10:20:36 -0300

    mudei

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -147,47 +147,47 @@ function lcd(){
 
 // --------------------- MENU LENTO
 
-const menuLinks = document.querySelectorAll('#menu a[href^="#"]');
+const menuLinks = document.querySelectorAll('#menu a[href^="#"]')
 
 function getDistanceFromTheTop(element) {
-  const id = element.getAttribute("href");
-  return document.querySelector(id).offsetTop;
+  const id = element.getAttribute("href")
+  return document.querySelector(id).offsetTop
 }
 
 function scrollToSection(event) {
-  event.preventDefault();
-  const distanceFromTheTop = getDistanceFromTheTop(event.target) - 90;
-  smoothScrollTo(0, distanceFromTheTop);
+  event.preventDefault()
+  const distanceFromTheTop = getDistanceFromTheTop(event.target) - 90
+  smoothScrollTo(0, distanceFromTheTop)
 }
 
 menuLinks.forEach((link) => {
-  link.addEventListener("click", scrollToSection);
+  link.addEventListener("click", scrollToSection)
 });
 
 function smoothScrollTo(endX, endY, duration) {
-  const startX = window.scrollX || window.pageXOffset;
-  const startY = window.scrollY || window.pageYOffset;
-  const distanceX = endX - startX;
-  const distanceY = endY - startY;
-  const startTime = new Date().getTime();
+  const startX = window.scrollX || window.pageXOffset
+  const startY = window.scrollY || window.pageYOffset
+  const distanceX = endX - startX
+  const distanceY = endY - startY
+  const startTime = new Date().getTime()
 
-  duration = typeof duration !== "undefined" ? duration : 400;
+  duration = typeof duration !== "undefined" ? duration : 400
 
   const easeInOutQuart = (time, from, distance, duration) => {
     if ((time /= duration / 2) < 1)
-      return (distance / 2) * time * time * time * time + from;
-    return (-distance / 2) * ((time -= 2) * time * time * time - 2) + from;
-  };
+      return (distance / 2) * time * time * time * time + from
+    return (-distance / 2) * ((time -= 2) * time * time * time - 2) + from
+  }
 
   const timer = setInterval(() => {
-    const time = new Date().getTime() - startTime;
-    const newX = easeInOutQuart(time, startX, distanceX, duration);
-    const newY = easeInOutQuart(time, startY, distanceY, duration);
+    const time = new Date().getTime() - startTime
+    const newX = easeInOutQuart(time, startX, distanceX, duration)
+    const newY = easeInOutQuart(time, startY, distanceY, duration)
     if (time >= duration) {
-      clearInterval(timer);
+      clearInterval(timer)
     }
-    window.scroll(newX, newY);
-  }, 1000 / 60);
+    window.scroll(newX, newY)
+  }, 1000 / 60)
 }
 
 //----------------------- MUDAR LOGOS PROJETO DESIGNER GRAFICO
@@ -196,7 +196,7 @@ function logos(imagem) {
   var limg = []
   
   for (var i = 1; i <= 2; i++) {
-    limg.push("imagens/DesignerGrafico/teste/" + imagem + i + ".png");
+    limg.push("imagens/DesignerGrafico/teste/" + imagem + i + ".png")
   }
   document.getElementById("if1").src = limg[0]
   document.getElementById("if2").src = limg[1]
